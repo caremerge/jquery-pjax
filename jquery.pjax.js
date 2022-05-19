a94f5308c520c109bc089383d1ff5a978e7de608
Muneeb Khawaja <muneeb@caremerge.com>
2022-05-19 17:00:36 +0500

    remove the iOS check for window.history - it's obsolete

--- jquery.pjax.js
+++ jquery.pjax.js
@@ -910,9 +910,7 @@ if ( $.inArray('state', $.event.props) < 0 )
 
 // Is pjax supported by this browser?
 $.support.pjax =
-  window.history && window.history.pushState && window.history.replaceState &&
-  // pushState isn't reliable on iOS until 5.
-  !navigator.userAgent.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]\D|WebApps\/.+CFNetwork)/)
+  window.history && window.history.pushState && window.history.replaceState
 
 $.support.pjax ? enable() : disable()
 
